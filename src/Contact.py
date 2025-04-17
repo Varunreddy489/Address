48a741d9c46c9086e56b573fc0677c47fd042898
Varunreddy <varunsannapureddy@gmail.com>
2025-04-17 10:38:55 +0530

    Add Contact class and initialixed attributes

---
+++ src/Contact.py
@@ -0,0 +1,14 @@
+class Contact:
+    def __init__(self):
+        self.first_name = None
+        self.last_name = None
+        self.address = None
+        self.city = None
+        self.state = None
+        self.zip_code = None
+        self.phone_number = None
+        self.email = None
+
+    def __repr__(self):
+        return (f"Contact({self.first_name}, {self.last_name}, {self.address}, "
+                f"{self.city}, {self.state}, {self.zip_code}, {self.phone_number}, {self.email})")
