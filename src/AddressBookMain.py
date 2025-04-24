3efc64ab55e5fb8dedc9ba8c491dd443391d2710
Varunreddy <varunsannapureddy@gmail.com>
2025-04-24 15:40:28 +0530

    Implemented Search Contacts by City or State

--- src/AddressBookMain.py
+++ src/AddressBookMain.py
@@ -1,3 +1,19 @@
+import AddressBook
+
+
 class AddressBookMain:
     def __init__(self):
         self.address_books = {}
+
+    def add_address_book(self, name):
+        """
+        Function to create a new address book
+        """
+        self.address_books[name] = AddressBook(name)
+        print(f"\nSuccessfully created Address Book - {name}")
+
+    def get_all_address_books(self):
+        """
+        To get all address books
+        """
+        return self.address_books
