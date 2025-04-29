97f806e1f7a83c05952b8f4459391b6825fddbb7
Varunreddy <varunsannapureddy@gmail.com>
2025-04-29 15:44:53 +0530

    Add Addredd Book Id to Schema

--- src/Contact.py
+++ src/Contact.py
@@ -1,18 +1,27 @@
 class Contact:
-    def __init__(
-        self, first_name, last_name, phone_number, address, city, state, zip_code, email
-    ):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip_code = zip_code
-        self.email = email
+    class Contact:
+        def __init__(
+            self,
+            first_name,
+            last_name,
+            email,
+            phone_number,
+            address,
+            city,
+            state,
+            zip_code,
+        ):
+            self.first_name = first_name
+            self.last_name = last_name
+            self.email = email
+            self.phone_number = phone_number
+            self.address = address
+            self.city = city
+            self.state = state
+            self.zip_code = zip_code
 
-    def __repr__(self):
-        return (
-            f"Contact({self.first_name}, {self.last_name}, {self.address}, "
-            f"{self.city}, {self.state}, {self.zip_code}, {self.phone_number}, {self.email})"
-        )
+        def __repr__(self):
+            return (
+                f"Contact({self.first_name}, {self.last_name}, {self.address}, "
+                f"{self.city}, {self.state}, {self.zip_code}, {self.phone_number}, {self.email})"
+            )
--- src/schema/schema.py
+++ src/schema/schema.py
@@ -2,14 +2,12 @@ from pydantic import BaseModel
 
 
 class ContactSchema(BaseModel):
+    address_book_id: int
     first_name: str
     last_name: str
+    email: str
+    phone_number: str
     address: str
     city: str
     state: str
     zip_code: str
-    phone_number: str
-    email: str
-
-
-
